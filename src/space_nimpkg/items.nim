1d793850d4fd593f62b68ae6e97c291897d94642
mollusk <silvernode@gmail.com>
2020-03-12 23:30:39 -0700

    Add worth parameter to certain items

--- src/space_nimpkg/items.nim
+++ src/space_nimpkg/items.nim
@@ -8,26 +8,26 @@ proc shipManual*(): Item =
 
     return shipManual
 
-proc fuel*(): Item =
+proc fuel*(worth: int): Item =
     var fuel = Item( name: "Fuel",
                     desc: "Used to power various machines including ships",
-                    worth: 70)
+                    worth: worth)
     return fuel
 
-proc food*(): Item =
+proc food*(worth: int): Item =
     var food = Item( name: "Food",
                     desc: "Grown across the solar system, a valuable resource",
-                    worth: 100)
+                    worth: worth)
     return food
 
-proc weapons*(): Item =
+proc weapons*(worth: int): Item =
     var weapons = Item( name: "Weapons",
                         desc: "Used for defense",
-                        worth: 150)
+                        worth: worth)
     return weapons
 
-proc ore*(): Item =
+proc ore*(worth: int): Item =
     var ore = Item( name: "Ore",
                     desc: "Used to build various types of structures",
-                    worth: 64)
+                    worth: worth)
     return ore
