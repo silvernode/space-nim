3f66ab712c7028b2f07753b0652f7caf39d874b1
mollusk <silvernode@gmail.com>
2020-03-12 23:09:34 -0700

    Create basic items

--- src/space_nimpkg/items.nim
+++ src/space_nimpkg/items.nim
@@ -1,9 +1,33 @@
 import entities
 
 
-proc book*(): Item =
-    var book = Item( name: "Ship Manual",
+proc shipManual*(): Item =
+    var shipManual = Item( name: "Ship Manual",
                     desc: "The manual for this ship (can be sold)",
                     worth: 50)
 
-    return book
+    return shipManual
+
+proc fuel*(): Item =
+    var fuel = Item( name: "Fuel",
+                    desc: "Used to power various machines including ships",
+                    worth: 70)
+    return fuel
+
+proc food*(): Item =
+    var food = Item( name: "Food",
+                    desc: "Grown across the solar system, a valuable resource",
+                    worth: 100)
+    return food
+
+proc weapons*(): Item =
+    var weapons = Item( name: "Weapons",
+                        desc: "Used for defense",
+                        worth: 150)
+    return weapons
+
+proc ore*(): Item =
+    var ore = Item( name: "Ore",
+                    desc: "Used to build various types of structures",
+                    worth: 64)
+    return ore
--- src/space_nimpkg/ships.nim
+++ src/space_nimpkg/ships.nim
@@ -3,6 +3,6 @@ import items
 
 proc jimi*(ship_name: string): Ship =
     var ship1 = Ship(name: ship_name,
-                     hp: 120,
-                     cargo: @[book()])
+                     power: 120,
+                     cargo: @[shipManual()])
     return ship1
