7c63aa066a984606dd591af49d12a46a56ac2ba9
mollusk <silvernode@gmail.com>
2020-03-16 07:42:27 -0700

    Export all ships

--- src/space_nimpkg/ships.nim
+++ src/space_nimpkg/ships.nim
@@ -6,14 +6,14 @@ var jimi* = Ship(name: "Jimi",
                 power: 120,
                 cargo: @[shipManual])
 
-var caroler = Ship(name: "Caroler",
+var caroler* = Ship(name: "Caroler",
                     power: 200,
                     cargo: @[shipManual])
 
-var yolen = Ship(name: "Yolen",
+var yolen* = Ship(name: "Yolen",
                 power: 225,
                 cargo: @[shipManual])
 
-var titan = Ship(name: "Titan",
+var titan* = Ship(name: "Titan",
                 power: 320,
                 cargo: @[shipManual])
