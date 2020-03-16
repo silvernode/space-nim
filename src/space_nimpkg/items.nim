99d0d6e138069ebc5caae59d65727c7f9def83a1
mollusk <silvernode@gmail.com>
2020-03-16 07:46:38 -0700

    Align attributes of items and ships

--- src/space_nimpkg/items.nim
+++ src/space_nimpkg/items.nim
@@ -2,27 +2,32 @@ import entities
 
 
 
-var shipManual* = Item( name: "Ship Manual",
-                desc: "The manual for this ship (can be sold)",
-                worth: 50)
+var shipManual* = Item( 
+    name: "Ship Manual",
+    desc: "The manual for this ship (can be sold)",
+    worth: 50)
 
 
-var fuel* = Item( name: "Fuel",
-                desc: "Used to power various machines including ships",
-                worth: 100)
+var fuel* = Item(
+    name: "Fuel",
+    desc: "Used to power various machines including ships",
+    worth: 100)
 
 
 
-var food* = Item( name: "Food",
-                desc: "Grown across the solar system, a valuable resource",
-                worth: 30)
+var food* = Item( 
+    name: "Food",
+    desc: "Grown across the solar system, a valuable resource",
+    worth: 30)
 
 
-var weapons* = Item( name: "Weapons",
-                    desc: "Used for defense",
-                    worth: 120)
+var weapons* = Item(
+    name: "Weapons",
+    desc: "Used for defense",
+    worth: 120)
 
 
-var ore* = Item( name: "Ore",
-                desc: "Used to build various types of structures",
-                worth: 115)
+var ore* = Item(
+    name: "Ore",
+    desc: "Used to build various types of structures",
+    worth: 115)
--- src/space_nimpkg/ships.nim
+++ src/space_nimpkg/ships.nim
@@ -2,18 +2,22 @@ import entities
 import items
 
 
-var jimi* = Ship(name: "Jimi",
-                power: 120,
-                cargo: @[shipManual])
+var jimi* = Ship(
+    name: "Jimi",
+    power: 120,
+    cargo: @[shipManual])
 
-var caroler* = Ship(name: "Caroler",
-                    power: 200,
-                    cargo: @[shipManual])
+var caroler* = Ship(
+    name: "Caroler",
+    power: 200,
+    cargo: @[shipManual])
 
-var yolen* = Ship(name: "Yolen",
-                power: 225,
-                cargo: @[shipManual])
+var yolen* = Ship(
+    name: "Yolen",
+    power: 225,
+    cargo: @[shipManual])
 
-var titan* = Ship(name: "Titan",
-                power: 320,
-                cargo: @[shipManual])
+var titan* = Ship(
+    name: "Titan",
+    power: 320,
+    cargo: @[shipManual])
