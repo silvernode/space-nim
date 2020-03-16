92f65e6091f35b9e721fe4815ebec2e13f9fd70f
mollusk <silvernode@gmail.com>
2020-03-16 09:25:05 -0700

    Add game loop prototype

--- src/space_nim.nim
+++ src/space_nim.nim
@@ -9,13 +9,13 @@ import space_nimpkg/ships
 import strutils
 
 when isMainModule:
-  var ship = jimi
+  
 
   var player: Character
   player = Character(name: "Dave",
                     location: "start",
                     credits: 200,
-                    ship: ship.name)
+                    ship: jimi)
 
   
   var station = solo
@@ -26,10 +26,29 @@ when isMainModule:
     echo (fmt"{player.name} has {player.credits} credits")
     echo (fmt"{player.name} is flying {player.ship}")
 
-    echo (fmt"Cargo for {ship.name}: ")
+    echo (fmt"Cargo for {player.ship.name}: ")
+
+    ShowInventory(player)
+
+    #echo(ship.cargo[0].name, ": ", "Worth: ", ship.cargo[0].worth, " credits: ", ship.cargo[0].desc, " Station: ", station.name)
+  var turn: int
+  turn = 0
+  var input: string
+  while true:
 
     
+    if turn == 0:
+      stdout.write("How many turns?: ")
+      input = readLine(stdin)
 
-    echo(ship.cargo[0].name, ": ", "Worth: ", ship.cargo[0].worth, " credits: ", ship.cargo[0].desc, " Station: ", station.name)
+      turn = input.parseInt()
+    
+    echo turn
 
-  test()
+    
+    if turn == 0:
+      break
+    if turn > 0:
+      turn = turn - 1
+
+    
