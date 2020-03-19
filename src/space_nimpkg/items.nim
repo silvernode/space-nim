2942899515f055bfb51e615c0fb8dd640c4c562d
mollusk <silvernode@gmail.com>
2020-03-19 13:18:54 -0700

    Add quantity property

--- src/space_nimpkg/items.nim
+++ src/space_nimpkg/items.nim
@@ -5,12 +5,14 @@ import entities
 var shipManual* = Item( 
     name: "Ship Manual",
     desc: "The manual for this ship (can be sold)",
+    quantity: 1,
     worth: 50)
 
 
 var fuel* = Item(
     name: "Fuel",
     desc: "Used to power various machines including ships",
+    quantity: 1,
     worth: 100)
 
 
@@ -18,16 +20,19 @@ var fuel* = Item(
 var food* = Item( 
     name: "Food",
     desc: "Grown across the solar system, a valuable resource",
+    quantity: 1,
     worth: 30)
 
 
 var weapons* = Item(
     name: "Weapons",
     desc: "Used for defense",
+    quantity: 1,
     worth: 120)
 
 
 var ore* = Item(
     name: "Ore",
     desc: "Used to build various types of structures",
+    quantity: 1,
     worth: 115)
