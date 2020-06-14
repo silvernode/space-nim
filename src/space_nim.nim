c6407eb1dcc7cb9144b87bcde1bd8813626f48a9
mollusk <silvernode@gmail.com>
2020-06-13 22:22:08 -0700

    Add main menu

--- src/space_nim.nim
+++ src/space_nim.nim
@@ -1,7 +1,53 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
 
-import space_nimpkg/submodule
+
+#import space_nimpkg/
+import terminal
+
+proc new_game() =
+    echo("A new game has started")
+
+proc load_game() =
+    echo("Open load game menu")
+
+proc options_menu() = 
+    echo("open options menu")
+
+proc exit_game() =
+    quit(0)
+
+proc main_menu() =
+    
+    while true:
+        echo("""
+        Space Nim
+        ----------
+
+        1) New Game
+        2) Load Game
+        3) Options
+        4) Exit
+
+        """)
+
+        var input = getch()
+        eraseScreen()
+        case input:
+
+        of '1':
+            new_game()
+
+        of '2':
+            load_game()
+
+        of '3':
+            options_menu()
+
+        of '4':
+            exit_game()
+
+        else:
+            eraseScreen()
 
 when isMainModule:
-  echo(getWelcomeMessage())
+  
+    main_menu()
