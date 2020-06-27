0e6bb5ccf3ebcd8e6cef855577d2173313eae1cc
mollusk <silvernode@gmail.com>
2020-06-27 11:59:33 -0700

    Create starting ships

---
+++ src/space_nimpkg/db.nim
@@ -0,0 +1,21 @@
+# SHIPS
+import objects
+var 
+    draco* = Ship(
+        name: "Draco",
+        attack: 2,
+        defense: 4
+    )
+
+    scythe* = Ship(
+        name: "Scythe",
+        attack: 4,
+        defense: 2
+
+    )
+
+    hobo* = Ship(
+        name: "Hobo",
+        attack:1,
+        defense: 5
+    )
